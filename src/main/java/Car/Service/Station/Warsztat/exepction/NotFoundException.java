bc7ba453971dc971f33cd087f60b1e7e266aed87
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-04-16 15:28:07 +0100

    Added basic project structure.

---
+++ src/main/java/Car/Service/Station/Warsztat/exepction/NotFoundException.java
@@ -0,0 +1,8 @@
+package Car.Service.Station.Warsztat.exepction;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
