9a3514a318af5d41c1b13ea7dcaffb9d7df9167b
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-04-17 16:14:24 +0100

    Finished StatusDao class.

--- src/main/java/Car/Service/Station/Warsztat/dao/StatusDao.java
+++ src/main/java/Car/Service/Station/Warsztat/dao/StatusDao.java
@@ -1,4 +1,93 @@
 package Car.Service.Station.Warsztat.dao;
 
+import Car.Service.Station.Warsztat.model.Customer;
+import Car.Service.Station.Warsztat.model.Status;
+import Car.Service.Station.Warsztat.util.DbUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatusDao {
+    private static final String READ_STATUS_QUERY = "SELECT * FROM status WHERE id = ?;";
+    private static final String READ_ALL_STATUS_QUERY = "SELECT * FROM status;";
+    private static final String CREATE_STATUS_QUERY = "INSERT INTO status(status, order_id) VALUES (?,?);";
+    private static final String UPDATE_STATUS_QUERY = "UPDATE status SET status status = ?, order_id = ? WHERE id = ?;";
+    private static final String DELETE_STATUS_QUERY = "DELETE FROM status WHERE id = ?;";
+
+    public Status read(int id) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(READ_STATUS_QUERY);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Status status = new Status();
+                status.setId(resultSet.getInt("id"));
+                status.setStatus(resultSet.getInt("status"));
+                status.setOrderId(resultSet.getInt("order_id"));
+                return status;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Status> readAll() {
+        List<Status> customerArrayList = new ArrayList<>();
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(READ_ALL_STATUS_QUERY);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Status status = new Status();
+                status.setId(resultSet.getInt("id"));
+                status.setStatus(resultSet.getInt("status"));
+                status.setOrderId(resultSet.getInt("order_id"));
+                customerArrayList.add(status);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customerArrayList;
+    }
+
+    public Status create(Status status) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement(CREATE_STATUS_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, status.getStatus());
+            statement.setInt(2, status.getOrderId());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                status.setId(resultSet.getInt(1));
+            }
+            return status;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void update(Status status) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_STATUS_QUERY);
+            statement.setInt(3, status.getId());
+            statement.setInt(1, status.getStatus());
+            statement.setInt(2, status.getOrderId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        try (Connection connection = DbUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_STATUS_QUERY)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
